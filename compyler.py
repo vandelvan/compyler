15eee97ae38744545fd662dc5cffb8da3bca6adc
vandelvan <vandel_van@hotmail.com>
2022-04-26 18:05:19 -0500

    Just relatives left to correct for p11

--- compyler.py
+++ compyler.py
@@ -5,11 +5,11 @@ def toInt(op):
     if op.isnumeric():
         return op
     if op.__contains__("$"):
-        return int(op.split("$")[1], 16)
+        return str(int(op.split("$")[1], 16))
     if op.__contains__("@"):
-        return int(op.split("@")[1], 8)
+        return str(int(op.split("@")[1], 8))
     if op.__contains__("%"):
-        return int(op.split("%")[1], 2)
+        return str(int(op.split("%")[1], 2))
 
 
 def idxgetn(op):
@@ -76,9 +76,11 @@ def idxcop(nformula, op):
 
 
 def searchinst(mnem, imm, op):
+    if op == None:
+        op = "0"
     loc = 0
-    hexop = int("0x0", 16)
     idxType = idxtype(op)
+    hexop = int("0x0", 16)
     if len(op) > 1:
         hexstr = "".join([n for n in op if n.isdigit()])
         if op.__contains__("$"):
@@ -105,170 +107,279 @@ def searchinst(mnem, imm, op):
                 else:
                     if hexop < 0x0 or (hexop > 0xFF**(int(inst[2])-1) and inst[1] != "EXT"):
                         return ["FDR", "", inst[2]]
+                    if inst[1] == "INH":
+                        op = ""
+                        hexop = ""
                     inst[0] += " "+op
                     inst[1] = inst[1]+"(LI="+inst[2]+")"
                     return inst
         return ["No encontrado", "", loc]
 
-
-with open("asms/P11.asm", "r") as asm:
-    out = []
-    outcl = []
-    loc = 0
-    vars = []
-    for line in asm:
-        # ignora comentarios
-        line = line.split(";")[0]
-        if line == "":
-            continue
-        instruccion = line.split()
-        imm = False
-        tag = ""
-        if line.__contains__(":"):
-            # etiqueta
-            tag = instruccion.pop(0)
-        if line.__contains__("ORG"):
-            outcl.append([str(loc), " ".join(instruccion), ""])
-            loc = hex(int("0x"+instruccion[1].split("$")[1], 16))
-            out.append(instruccion)
-            continue
-        if line.__contains__("START"):
-            instruccion.insert(0, str(loc))
-            outcl.append([str(loc), " ".join(instruccion), ""])
-            loc = hex(int("0x0", 16))
-            out.append(instruccion)
-            continue
-        if line.__contains__("EQU"):
-            vars.append({instruccion[0]: instruccion[2]})
-            out.append(instruccion)
-            outcl.append([str(loc), " ".join(instruccion), ""])
-            continue
-        if line.__contains__("DC."):
-            s = 0
-            ops = []
-            if len(instruccion) > 1:
+locs = ["0x0"]
+vars = [] 
+def paso1():
+    i = 0
+    with open("asms/P11.asm", "r") as asm:
+        for line in asm:
+            i+=1
+            if len(locs) < i:
+                locs.append(locs[-1])
+            prevloc = locs[-1] if i > 0 else 0
+            # ignora comentarios
+            line = line.split(";")[0]
+            if line == "":
+                continue
+            instruccion = line.split()
+            imm = False
+            if line.__contains__("ORG"):
+                locs.append(hex(int("0x"+instruccion[1].split("$")[1], 16)))
+                continue
+            if line.__contains__("START"):
+                locs.append(hex(int("0x0", 16)))
+                continue
+            if line.__contains__("EQU"):
+                vars.append({instruccion[0]: instruccion[2]})
+                continue
+            if line.__contains__("DC."):
+                s = 0
+                ops = []
+                if len(instruccion) > 1:
+                    ops = instruccion[1].split(",")
+                if line.__contains__("DC.B"):
+                    for i in range(len(ops)):
+                        s = s+1
+                        ops[i] = hex(int(ops[i]))
+                    if not ops:
+                        s = 1
+                        ops = ["00"]
+                if line.__contains__("DC.W"):
+                    for i in range(len(ops)):
+                        s = s+2
+                        ops[i] = hex(int(ops[i]))
+                    if not ops:
+                        s = 2
+                        ops = ["00", "00"]
+                locs.append(hex(int(prevloc, 16)+int(str(s), 16)))
+                continue
+            if line.__contains__("BSZ") or line.__contains__("ZMB"):
+                ops = []
+                for i in range(int(instruccion[2])):
+                    ops.append("00")
+                locs.append(hex(int(prevloc, 16)+int(instruccion[2])))
+                continue
+            if line.__contains__("FCB"):
+                s = 0
                 ops = instruccion[1].split(",")
-            if line.__contains__("DC.B"):
                 for i in range(len(ops)):
                     s = s+1
                     ops[i] = hex(int(ops[i]))
-                if not ops:
-                    s = 1
-                    ops = ["00"]
-            if line.__contains__("DC.W"):
+                locs.append(hex(int(prevloc, 16)+int(str(s), 16)))
+                continue
+            if line.__contains__("FCC"):
+                cc = instruccion[1].split("/")[1]
+                ops = []
+                sum = 0
+                for i in cc:
+                    op = int(str(ord(i)))
+                    ops.append(hex(op))
+                    sum = sum+op
+                locs.append(hex(int(prevloc, 16)+sum))
+                continue
+            if line.__contains__("FILL"):
+                lc = instruccion[1].split(",")
+                ops = []
+                for i in range(int(lc[1])):
+                    ops.append(hex(int(lc[0])))
+                locs.append(hex(int(prevloc, 16)+int(str(lc[1]), 16)))
+                continue
+            inst = searchinst(instruccion[0], imm, "0")
+            inst.insert(0, str(prevloc))
+            op = instruccion[1] if len(instruccion) > 1 else ""
+            idxType = idxtype(op)
+            if line.__contains__("#"):
+                imm = True
+                op = op.replace("#","")
+            if idxType == 0:
+                op = toInt(op)
+            inst = searchinst(instruccion[0], imm, op)
+            if inst[0] == "No encontrado":
+                vars.append({instruccion.pop(0): prevloc})
+                if len(instruccion)>1:
+                    op = instruccion[1] if len(instruccion) > 1 else ""
+                    idxType = idxtype(op)
+                    if op.__contains__("#"):
+                        imm = True
+                        op = op.replace("#","")
+                    if idxType == 0:
+                        op = toInt(op)
+                    inst = searchinst(instruccion[0], imm, op)
+                    if inst[0] == "No encontrado":
+                        continue
+                else:                    
+                    continue
+            if idxType != 0:
+                op = idxcop(idxType, op)
+            if op == "" or op == None:
+                op = "0"
+            locs.append(hex(int(prevloc, 16)+int(inst[2] if len(inst) > 2 else 0, 16)))
+            if inst[1] != "FDR":
+                inst.pop()
+                inst.pop()
+
+def paso2():
+    out = []
+    outcl = []
+    i = -1
+    with open("asms/P11.asm", "r") as asm:
+        for line in asm:
+            i+=1
+            # ignora comentarios
+            line = line.split(";")[0]
+            if line == "":
+                continue
+            instruccion = line.split()
+            imm = False
+            tag = ""
+            if line.__contains__(":"):
+                # etiqueta
+                tag = instruccion.pop(0)
+            if line.__contains__("ORG"):
+                outcl.append([str(locs[i]), " ".join(instruccion), ""])
+                instruccion.insert(0, str(locs[i]))
+                out.append(instruccion)
+                continue
+            if line.__contains__("START"):
+                outcl.append([str(locs[i]), " ".join(instruccion), ""])
+                instruccion.insert(0, str(locs[i]))
+                out.append(instruccion)
+                continue
+            if line.__contains__("EQU"):
+                outcl.append([str(locs[i]), " ".join(instruccion), ""])
+                instruccion.insert(0, str(locs[i]))
+                out.append(instruccion)
+                continue
+            if line.__contains__("DC."):
+                s = 0
+                ops = []
+                if len(instruccion) > 1:
+                    ops = instruccion[1].split(",")
+                if line.__contains__("DC.B"):
+                    for i in range(len(ops)):
+                        s = s+1
+                        ops[i] = hex(int(ops[i]))
+                    if not ops:
+                        s = 1
+                        ops = ["00"]
+                if line.__contains__("DC.W"):
+                    for i in range(len(ops)):
+                        s = s+2
+                        ops[i] = hex(int(ops[i]))
+                    if not ops:
+                        s = 2
+                        ops = ["00", "00"]
+                instruccion.insert(0, str(locs[i]))
+                outcl.append([str(locs[i]), " ".join(instruccion), " ".join(ops)])
+                out.append(instruccion)
+                continue
+            if line.__contains__("BSZ") or line.__contains__("ZMB"):
+                instruccion.insert(0, str(locs[i]))
+                ops = []
+                for i in range(int(instruccion[2])):
+                    ops.append("00")
+                outcl.append([str(locs[i]), " ".join(instruccion), " ".join(ops)])
+                out.append(instruccion)
+                continue
+            if line.__contains__("FCB"):
+                s = 0
+                ops = instruccion[1].split(",")
                 for i in range(len(ops)):
-                    s = s+2
+                    s = s+1
                     ops[i] = hex(int(ops[i]))
-                if not ops:
-                    s = 2
-                    ops = ["00", "00"]
-            instruccion.insert(0, str(loc))
-            outcl.append([str(loc), " ".join(instruccion), " ".join(ops)])
-            loc = hex(int(loc, 16)+int(str(s), 16))
-            out.append(instruccion)
-            continue
-        if line.__contains__("BSZ") or line.__contains__("ZMB"):
-            instruccion.insert(0, str(loc))
-            ops = []
-            for i in range(int(instruccion[2])):
-                ops.append("00")
-            outcl.append([str(loc), " ".join(instruccion), " ".join(ops)])
-            loc = hex(int(loc, 16)+int(instruccion[2]))
-            out.append(instruccion)
-            continue
-        if line.__contains__("FCB"):
-            s = 0
-            ops = instruccion[1].split(",")
-            for i in range(len(ops)):
-                s = s+1
-                ops[i] = hex(int(ops[i]))
-            instruccion.insert(0, str(loc))
-            outcl.append([str(loc), " ".join(instruccion), " ".join(ops)])
-            loc = hex(int(loc, 16)+int(str(s), 16))
-            out.append(instruccion)
-            continue
-        if line.__contains__("FCC"):
-            cc = instruccion[1].split("/")[1]
-            instruccion.insert(0, str(loc))
-            ops = []
-            sum = 0
-            for i in cc:
-                op = int(str(ord(i)))
-                ops.append(hex(op))
-                sum = sum+op
-            outcl.append([str(loc), " ".join(instruccion), " ".join(ops)])
-            loc = hex(int(loc, 16)+sum)
-            out.append(instruccion)
-            continue
-        if line.__contains__("FILL"):
-            lc = instruccion[1].split(",")
-            ops = []
-            for i in range(int(lc[1])):
-                ops.append(hex(int(lc[0])))
-            instruccion.insert(0, str(loc))
-            outcl.append([str(loc), " ".join(instruccion), " ".join(ops)])
-            loc = hex(int(loc, 16)+int(str(lc[1]), 16))
-            out.append(instruccion)
-            continue
-        inst = searchinst(instruccion[0], imm, "0")
-        if inst[0] == "No encontrado":
-            vars.append({instruccion.pop(0): str(instruccion)})
-            continue
-        if line.__contains__("END"):
-            instruccion.insert(0, str(loc))
-            out.append(instruccion)
-            outcl.append([str(loc), " ".join(instruccion), ""])
-            break
-        op = instruccion[1] if len(instruccion) > 1 else ""
-        idxType = idxtype(op)
-        if line.__contains__("#"):
-            imm = True
-            op = op.split("#")[1]
-        if idxType == 0:
-            op = toInt(op)
-        for i in vars:
-            if op in i:
-                op = toInt(i[op])
-        inst = searchinst(instruccion[0], imm, op)
-        if inst[0] == "No encontrado":
-            vars.append({instruccion.pop(0): str(instruccion)})
-            continue
-        inst.insert(0, str(loc))
-        if idxType != 0:
-            op = idxcop(idxType, op)
-        if op == "":
-            op = "0"
-        outcl.append([str(loc), " ".join(instruccion), inst[4],hex(int(op))])
-        loc = hex(int(loc, 16)+int(inst[3] if len(inst) > 2 else 0, 16))
-        if inst[1] != "FDR":
-            inst.pop()
-            inst.pop()
-        out.append(inst)
+                instruccion.insert(0, str(locs[i]))
+                outcl.append([str(locs[i]), " ".join(instruccion), " ".join(ops)])
+                out.append(instruccion)
+                continue
+            if line.__contains__("FCC"):
+                cc = instruccion[1].split("/")[1]
+                instruccion.insert(0, str(locs[i]))
+                ops = []
+                sum = 0
+                for i in cc:
+                    op = int(str(ord(i)))
+                    ops.append(hex(op))
+                    sum = sum+op
+                outcl.append([str(locs[i]), " ".join(instruccion), " ".join(ops)])
+                out.append(instruccion)
+                continue
+            if line.__contains__("FILL"):
+                lc = instruccion[1].split(",")
+                ops = []
+                for i in range(int(lc[1])):
+                    ops.append(hex(int(lc[0])))
+                instruccion.insert(0, str(locs[i]))
+                outcl.append([str(locs[i]), " ".join(instruccion), " ".join(ops)])
+                out.append(instruccion)
+                continue
+            inst = searchinst(instruccion[0], imm, "0")
+            if inst[0] == "No encontrado":
+                outcl.append([str(locs[i]), " ".join(instruccion), " "])
+                instruccion.insert(0, str(locs[i]))
+                out.append(instruccion)
+                continue
+            if line.__contains__("END"):
+                instruccion.insert(0, str(locs[i]))
+                out.append(instruccion)
+                outcl.append([str(locs[i]), " ".join(instruccion), ""])
+                break
+            op = instruccion[1] if len(instruccion) > 1 else ""
+            idxType = idxtype(op)
+            if line.__contains__("#"):
+                imm = True
+                op = op.split("#")[1]
+            if idxType == 0:
+                op = toInt(op)
+            for v in vars:
+                if op in v:
+                    op = toInt(v[op])
+            inst = searchinst(instruccion[0], imm, op)
+            inst.insert(0, str(locs[i]))
+            if idxType != 0:
+                op = idxcop(idxType, op)
+            if op == "" or op == None:
+                op = "0"
+            outcl.append([str(locs[i]), " ".join(instruccion), inst[4],hex(int(op)) if not inst[2].__contains__("INH") else ""])
+            if inst[1] != "FDR":
+                inst.pop()
+                inst.pop()
+            out.append(inst)
 
-    with open("lsts/P11.lst", "w") as lst:
-        for line in out:
-            for part in line:
-                if part.__contains__("0x"):
-                    part = part.replace("0x", "")
-                    part = part.zfill(4)
-                lst.write(part+"\t")
-            lst.write("\n")
-
-    with open("contlocs/P11.contloc", "w") as contloc:
-        contloc.write("contloc\tLinea de programa\tcop\n")
-        for line in outcl:
-            for part in line:
-                if part.__contains__("0x"):
-                    part = part.replace("0x", "")
-                    if len(part) > 2:
+        with open("lsts/P11.lst", "w") as lst:
+            for line in out:
+                for part in line:
+                    if part.__contains__("0x"):
+                        part = part.replace("0x", "")
                         part = part.zfill(4)
-                    else:
-                        part = part.zfill(2)
-                contloc.write(part+"\t\t\t")
-            contloc.write("\n")
+                    lst.write(part+"\t")
+                lst.write("\n")
 
-    with open("tabsims/P11.tabsim", "w") as tabsim:
-        for line in vars:
-            for i in line:
-                tabsim.write(i+": "+line[i])
-            tabsim.write("\n")
+        with open("contlocs/P11.contloc", "w") as contloc:
+            contloc.write("contloc\tLinea de programa\tcop\n")
+            for line in outcl:
+                for part in line:
+                    if part.__contains__("0x"):
+                        part = part.replace("0x", "")
+                        if len(part) > 2 or part == line[0].replace("0x", ""):
+                            part = part.zfill(4)
+                        else:
+                            part = part.zfill(2)
+                    contloc.write(part+"\t\t\t")
+                contloc.write("\n")
+
+        with open("tabsims/P11.tabsim", "w") as tabsim:
+            for line in vars:
+                for i in line:
+                    tabsim.write(i+": "+line[i])
+                tabsim.write("\n")
+
+paso1()
+paso2()
